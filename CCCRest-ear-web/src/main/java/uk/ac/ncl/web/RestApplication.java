4a42b3bdc7a0bf74ded766175f288a9a10f7aaff
Ioannis Sfyrakis <giannisfyrakis@gmail.com>
2018-08-11 11:30:09 +0100

    add code for web component

---
+++ CCCRest-ear-web/src/main/java/uk/ac/ncl/web/RestApplication.java
@@ -0,0 +1,12 @@
+/*
+ *
+ */
+package uk.ac.ncl.web;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/services")
+public class RestApplication extends Application {
+
+}
